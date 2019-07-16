e8cb88acf9fd889c72b507903fec15a58d509bc6
Nick Vannoy <svannoy6@gmail.com>
2019-07-16 15:46:32 -0500

    Calculator class method added.

--- CalculatorWithTests/Calculator.cs
+++ CalculatorWithTests/Calculator.cs
@@ -3,6 +3,35 @@ namespace CalculatorWithTests
 {
     class Calculator
     {
-        
+        public decimal Add { get; set; }
+        public decimal Subtract { get; set; }
+        public decimal Multiply { get; set; }
+        public decimal Divide { get; set; }
+        public decimal Factorial { get; set; }
+
+        public decimal GetAddition()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetSubtraction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetMultiplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetDivision()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetFactorial()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
