d1302c374bb08cf82a0fadf12274ff246cf3522d
Nick Vannoy <svannoy6@gmail.com>
2019-07-17 11:17:57 -0500

    Complete and runs successfully.

--- CalculatorWithTests/Program.cs
+++ CalculatorWithTests/Program.cs
@@ -16,13 +16,19 @@ namespace CalculatorWithTests
             return answer;
         }
 
+        static string UsersChoiceResponse(string answer)
+        {
+            string usersAnswerChoice = Console.ReadLine();
+            return usersAnswerChoice;
+        }
+
 
         static void Main(string[] args)
         {
             Calculator test = new Calculator();
 
-            Console.WriteLine("What number would you like to use for our test?");
-            string firstNumberResponse = Console.ReadLine();
+            //Console.WriteLine("What number would you like to use for our test?");
+            //string firstNumberResponse = Console.ReadLine();
             test.UsersFirstNumber = GetDecimalAnswerFromUser("What number would you like to use for our test?");
 
 
@@ -30,55 +36,63 @@ namespace CalculatorWithTests
 
             while (usersChoice != "Q")
             {
-                Console.WriteLine("What would you like to do with this number?");
+                Console.WriteLine("What would you like to do with this number:");
+
                 Console.WriteLine("Add?");
                 Console.WriteLine("Subtract?");
                 Console.WriteLine("Multiply?");
                 Console.WriteLine("Divide?");
                 Console.WriteLine("Factorial?");
 
-                usersChoice = Console.ReadLine();
-
-
-                if (usersChoice.ToUpper() == "Add")
+                string usersAnswerChoice = Console.ReadLine();
+                    
+        
+                if (usersAnswerChoice.ToUpper() == "ADD")
                 {
                     test.Addition = GetDecimalAnswerFromUser("How much would you like to add to this number?");
                     decimal additionTotal = test.GetAdditionTotal();
 
-                    Console.WriteLine(additionTotal);
+
+                    Console.WriteLine(" " + test.UsersFirstNumber + " + " + test.Addition + " = " + additionTotal + "");
                 }
-                else if (usersChoice.ToUpper() == "Subtract")
+                else if (usersAnswerChoice.ToUpper() == "SUBTRACT")
                 {
                     test.Subtraction = GetDecimalAnswerFromUser("How much would you like to subtract from this number?");
                     decimal subtractionTotal = test.GetSubtractionTotal();
 
-                    Console.WriteLine(subtractionTotal);
+
+                    Console.WriteLine(" " + test.UsersFirstNumber + " - " + test.Subtraction + " = " + subtractionTotal + "");
                 }
-                else if (usersChoice.ToUpper() == "Multiply")
+                else if (usersAnswerChoice.ToUpper() == "MULTIPLY")
                 {
                     test.Multiplication = GetDecimalAnswerFromUser("okay, by what number?");
                     decimal multiplicationTotal = test.GetMultiplicationTotal();
 
-                    Console.WriteLine(multiplicationTotal);
+
+                    Console.WriteLine(" " + test.UsersFirstNumber + " * " + test.Multiplication + " = " + multiplicationTotal + "");
                 }
-                else if (usersChoice.ToUpper() == "Divide") 
+                else if (usersAnswerChoice.ToUpper() == "DIVIDE") 
                 {
                     test.Division = GetDecimalAnswerFromUser("by how much?");
                     decimal divisionTotal = test.GetDivisionTotal();
 
-                    Console.WriteLine(divisionTotal);
+                    
+                    Console.WriteLine(" " + test.UsersFirstNumber + " / " + test.Division + " = " + divisionTotal + "");
                 }
-                else if(usersChoice.ToUpper() == "Factorial")
+                else if(usersAnswerChoice.ToUpper() == "FACTORIAL")
                 {
                     int firstUsersNumber = decimal.ToInt32(test.UsersFirstNumber);
                     int factorTotal;
 
                     factorTotal = test.GetFactorialTotal(firstUsersNumber);
 
-                    Console.WriteLine(factorTotal);
+
+                    Console.WriteLine("The factor total for " + test.UsersFirstNumber + " is " + factorTotal + "!");
 
                 }
-                
+
+                Console.WriteLine("The test is finished running!");
+                return;
             }
             
 
